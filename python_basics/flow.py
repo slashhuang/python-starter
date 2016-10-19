2265559bcf5c7e6f55ad6e8bfd18ec500b282b30
slashhuang <627284482@qq.com>
2016-10-19 23:52:35 +0800

    add while for range and int()

--- python_basics/flow.py
+++ python_basics/flow.py
@@ -6,10 +6,28 @@ elif age==15:
 else:
 	print('hello world')
 
-_i = input('plese input your age:')
+_i = '2'
 # 基本所有语言都有基本类型的转义规则，动态语言尤其
 birth = int(_i)
+# 条件语句
 if birth>20:
 	print('u are an adult')
 else:
 	print('u are student')	
+
+# 循环语句
+names = ['1',2,3]
+
+# range函数生成正数序列
+for name in names:
+ 	print(name)
+print(list(range(5)))	
+
+# while循环
+count=10
+sum=0
+while(count>0):
+	sum+=count
+	count=count-1
+print(sum)	
+
