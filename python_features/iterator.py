4c5776add4528fa696b0d08bf0ee1bc85c3fd606
slashhuang <627284482@qq.com>
2016-11-08 00:02:50 +0800

    modify iterable

--- python_features/iterator.py
+++ python_features/iterator.py
@@ -17,3 +17,8 @@ for i,value in enumerate(e): # 将list列表转化索引元素对
 from collections import Iterable
  
 print(isinstance([1,2],Iterable)) # True
+
+colors = ["red", "green", "blue", "purple"]
+ratios = [0.2, 0.3, 0.1, 0.4]
+for color, ratio in zip(colors, ratios):
+    print("{}% {}".format(ratio * 100, color))
