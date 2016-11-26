e1802213a6c09c1a6d2853c9ec77417871b0c9ef
slashhuang <627284482@qq.com>
2016-11-26 20:48:55 +0800

    add private

---
+++ python_oop/private.py
@@ -0,0 +1,27 @@
+# 如果要让内部属性不被外部访问，可以把属性的名称前加上两个下划线__，在Python中，
+# 实例的变量名如果以__开头，就变成了一个私有变量（private），
+# 只有内部可以访问，外部不能访问
+
+class Student():
+	def __init__(self,name,score):
+		self.__name=name
+		self.__score=score
+	def print_score(self):
+		print('%s has score %s'% (self.__name,self.__score))
+	def set_score(self,score):
+		# boolean运算 or and or
+		if	score>=10 and score<=100:
+			self.__score=score
+		else:
+			# 抛错
+			raise ValueError('bad score')	
+
+
+a = Student('slashhuang',100)
+a.print_score()
+
+a.set_score(99)
+a.print_score()
+
+a.set_score(19)
+a.print_score()
